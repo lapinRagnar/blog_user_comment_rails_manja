6609ba01be484616324c47f646349b2b178127b9
lapinragnar <lapinragnar@gmail.com>
2020-12-08 21:26:15 +0100

    installation de post user comment avec refresh et ca marche nicker avec validation de commenaire

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  devise_for :users
+  
+  resources :posts do
+    post 'comments', to: 'comments#create'
+  end
+
+
+  root 'posts#index'
+
+  
 end
