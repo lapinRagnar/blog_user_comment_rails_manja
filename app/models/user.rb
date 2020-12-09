3a02945cd38b14a9b356bd0399166743e1e67d4d
lapinragnar <lapinragnar@gmail.com>
2020-12-09 07:12:25 +0100

    ajout d'image user suite

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,36 @@ class User < ApplicationRecord
 
   has_many :comments, dependent: :destroy
 
+  has_one_attached :avatar
+  after_commit :add_default_avatar, on: %i[create update]
+
   def username
     return email.split('@')[0].capitalize
+  end
+
+  def avatar_thumbnail
+    if avatar.attached?
+      avatar.variant(resize: "150x150!").processed 
+    else
+      "/default_profile.jpg"
+    end
+
+  end
+
+
+  private
+
+  def add_default_avatar
+    unless avatar.attached?
+      avatar.attach(
+        io: File.open(
+          Rails.root.join(
+            'app', 'assets', 'images', 'default_profile.jpg'
+          )
+        ), filename: "default_profile.jpg",
+        content_type: 'image/jpg'
+      )
+    end
   end
 
 end
