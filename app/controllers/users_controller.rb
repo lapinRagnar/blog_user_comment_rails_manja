cf05f98a45529606873225deedd9c1c09e3fed95
lapinragnar <lapinragnar@gmail.com>
2020-12-09 03:54:55 +0100

    navbar bootstrap avec condition current_user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  get 'users/index'
   devise_for :users
   
   resources :posts do
